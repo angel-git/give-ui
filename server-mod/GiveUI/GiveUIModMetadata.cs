0fee0cdd803ca3c45687a34432a18f2ca1f84df5
agavalda <agavalda@digital.ai>
2025-08-03 11:41:26 +0200

    #172 update package and metadata changes

--- server-mod/GiveUI/GiveUIModMetadata.cs
+++ server-mod/GiveUI/GiveUIModMetadata.cs
@@ -4,17 +4,17 @@ namespace GiveUI;
 
 public record GiveUIModMetadata : AbstractModMetadata
 {
-    public override string ModId { get; set; } = "com.agavalda.giveui";
-    public override string Name { get; set; } = "give-ui";
-    public override string Author { get; set; } = "agavalda";
+    public override string ModGuid { get; init; } = "com.agavalda.giveui";
+    public override string Name { get; init; } = "give-ui";
+    public override string Author { get; init; } = "agavalda";
     public override List<string>? Contributors { get; set; }
-    public override string Version { get; set; } = "4.0.0";
-    public override string SptVersion { get; set; } = "4.0";
+    public override string Version { get; init; } = "4.0.0";
+    public override string SptVersion { get; init; } = "4.0";
     public override List<string>? LoadBefore { get; set; }
     public override List<string>? LoadAfter { get; set; }
     public override List<string>? Incompatibilities { get; set; }
     public override Dictionary<string, string>? ModDependencies { get; set; }
     public override string? Url { get; set; }
     public override bool? IsBundleMod { get; set; }
-    public override string? Licence { get; set; } = "MIT";
+    public override string License { get; init; } = "MIT";
 }
